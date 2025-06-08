9ba244755dc5af945005f65d5768f7df1c78f70f
A5R13L <asriel@asriel.dev>
2025-06-07 21:51:16 -0400

    enable the auto complete enter functionality and add dismissial action.

--- src/suggestionFix.ts
+++ src/suggestionFix.ts
@@ -1,37 +1,20 @@
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 import { SuggestController } from "./glua/definitions";
 
-enum SuggestState {
-    Hidden = 0,
-    Showing = 3,
-}
-
 export async function ImplementSuggestionFix(): Promise<void> {
-    editor?.addCommand(monaco.KeyCode.Escape, () => {
-        const suggest = editor?.getContribution(
-            "editor.contrib.suggestController",
-        ) as unknown as SuggestController;
+    editor?.addAction({
+        id: "dismissSuggestion",
+        label: "Dismiss Suggestion",
+        keybindings: [monaco.KeyCode.Escape],
+        precondition: "suggestWidgetVisible",
+        run: () => {
+            const suggest = editor?.getContribution(
+                "editor.contrib.suggestController",
+            ) as unknown as SuggestController;
 
-        if (suggest && suggest.widget._value._state === SuggestState.Showing) {
-            suggest?.cancelSuggestWidget();
-
-            return;
-        }
-
-        editor?.trigger("keyboard", "type", { text: "" });
-    });
-
-    editor?.addCommand(monaco.KeyCode.Enter, () => {
-        const suggest = editor?.getContribution(
-            "editor.contrib.suggestController",
-        ) as unknown as SuggestController;
-
-        if (suggest && suggest.widget._value._state === SuggestState.Showing) {
-            suggest?.acceptSelectedSuggestion();
-
-            return;
-        }
-
-        editor?.trigger("keyboard", "type", { text: "\n" });
+            if (suggest) {
+                suggest.cancelSuggestWidget();
+            }
+        },
     });
 }
